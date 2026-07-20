c9200cb81d538c3b1ba048d898480b3c810aa170
Lorena <lorena.akci@gmail.com>
2026-07-20 21:12:22 +0200

    class Main ispis

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static void main (String[] args) {
+
+        Krug krug = new Krug("Plavi krug", 6);
+        Trokut trokut = new Trokut("Crveni trokut", 9, 7, 5);
+        Pravokutnik pravokutnik = new Pravokutnik("Zeleni pravokutnik", 6, 12);
+
+        System.out.println("Naziv: " + krug.getNaziv() + ", Opseg: " + krug.opseg() + ", Površina: " + krug.povrsina());
+        System.out.println("Naziv: " + trokut.getNaziv() + ", Opseg: " + trokut.opseg() + ", Površina: " + trokut.povrsina());
+        System.out.println("Naziv: " + pravokutnik.getNaziv() + ", Opseg: " + pravokutnik.opseg() + ", Površina: " + pravokutnik.povrsina());
+    }
+}
